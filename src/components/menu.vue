7580d7afdc1dcf4c05f72cf5e85a80a6ba78d295
余宝彬 <502275531@qq.com>
2019-03-15 22:12:17 +0800

    解决错位问题

--- src/components/menu.vue
+++ src/components/menu.vue
@@ -15,6 +15,7 @@
   name: 'drop-menu',
   data() {
     return {
+      elm: null,
       h: 'drop-menu-left',
       v: 'drop-menu-top',
       maxW: 120,
@@ -33,6 +34,7 @@
       colors: [],
       changeColor: [],
       activeObj: {},
+      cssStyle: {}
     }
   },
   created() {
@@ -105,27 +107,51 @@
       const index = this.list.findIndex(elem => elem.name === newValue.name);
       this.changeColor = [...this.colors];
       this.changeColor[index] = this.activeColor;
+    },
+    show(newValue) {
+      if (newValue) {
+        const elmOffset = this.elm.getBoundingClientRect();
+        const winW = document.documentElement.clientWidth;
+        const winH = document.documentElement.clientHeight;
+        const parent = this.elm.parentNode;
+        const parentW = parent.offsetWidth;
+        const parentH = parent.offsetHeight;
+        const elmX = elmOffset.x | elmOffset.left;
+        const elmY = elmOffset.y | elmOffset.top;
+        const elmLeft = this.elm.offsetLeft;
+        const elmTop = this.elm.offsetTop;
+        const elmW = this.elm.clientWidth;
+        const elmH = this.elm.clientHeight;
+
+        let top = `${this.elmTop + this.elmH + 8}px`;
+        let vertical = {};
+        let horizontal = {};
+        let normalCss = {};
+        if (elmX + this.maxW < winW) {
+          this.h = 'drop-menu-left';
+          horizontal = { left: `${elmLeft + (elmW / 2 - 10) }px`}
+        }else {
+          this.h = 'drop-menu-right';
+          horizontal = { right: `${parentW - elmLeft - (elmW / 2) - 10 }px`};
+        }
+        if (elmY + elmH + this.maxH < winH) {
+          this.v = 'drop-menu-top';
+          vertical = { top : `${elmTop + elmH + 8}px`};
+        } else {
+          this.v = 'drop-menu-down';
+          vertical = { bottom : `${parentH - elmTop + 10}px`};
+        }
+        normalCss = { 'background-color': this.bgColor, 'border-radius': this.radius }
+        this.cssStyle = Object.assign(horizontal, vertical, normalCss);
+      }
     }
   },
   mounted() {
     if (!this.target) {
       throw new Error('Please setup the target attr !');
     }else {
-      const elm = document.querySelector(this.target);
-      if (elm) {
-        const elmOffset = elm.getBoundingClientRect();
-        this.winW = document.documentElement.clientWidth;
-        this.winH = document.documentElement.clientHeight;
-        const parent = elm.parentNode;
-        this.parentW = parent.offsetWidth;
-        this.parentH = parent.offsetHeight;
-        this.elmX = elmOffset.x | elmOffset.left;
-        this.elmY = elmOffset.y | elmOffset.top;
-        this.elmLeft = elm.offsetLeft;
-        this.elmTop = elm.offsetTop;
-        this.elmW = elm.clientWidth;
-        this.elmH = elm.clientHeight;
-      }else {
+      this.elm = document.querySelector(this.target);
+      if (!this.elm) {
         throw new Error('target not found!');
       }
     }
@@ -150,31 +176,7 @@
       }
       return (m + 1);
     }
-  },
-  computed: {
-  	cssStyle() {
-  	  let top = `${this.elmTop + this.elmH + 8}px`;
-      let vertical = {};
-  	  let horizontal = {};
-  	  let normalCss = {};
-      if (this.elmX + this.maxW < this.winW) {
-        this.h = 'drop-menu-left';
-      	horizontal = { left: `${this.elmLeft + (this.elmW / 2 - 10) }px`}
-      }else {
-      	this.h = 'drop-menu-right';
-        horizontal = { right: `${this.parentW - this.elmLeft - (this.elmW / 2) - 10 }px`};
-      }
-      if (this.elmY + this.elmH + this.maxH < this.winH) {
-        this.v = 'drop-menu-top';
-        vertical = { top : `${this.elmTop + this.elmH + 8}px`};
-      } else {
-        this.v = 'drop-menu-down';
-        vertical = { bottom : `${this.parentH - this.elmTop + 10}px`};
-      }
-      normalCss = { 'background-color': this.bgColor, 'border-radius': this.radius }
-      return Object.assign(horizontal, vertical, normalCss);
-  	},
-  },
+  }
 }
 </script>
 <style scoped>
